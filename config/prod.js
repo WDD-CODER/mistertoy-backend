ed5f020ed5e5c5db6778a983d9f0e36c7faa88e1
Your Name <dan.weibren@gmail.com>
2025-09-21 11:27:48 +0300

    Connected the application back end to Mongodb Atlas.

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+    dbURL: 'mongodb+srv://dan:123@cluster0.scxfhxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     dbName: 'tester_db',
 }
